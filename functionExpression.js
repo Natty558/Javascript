4632b8e19d66d0ed90dd02d4060e6ba3448fe056
Natty558 <anabanathaniel099@gmail.com>
2026-06-23 16:50:27 -0700

    Add files via upload

---
+++ functionExpression.js
@@ -0,0 +1,12 @@
+const calculateBill = function(months) {
+    let total = months * 10;
+
+    if (months >= 12) {
+        total -= 20;
+    }
+
+    return total;
+};
+
+console.log(calculateBill(6));   // 60
+console.log(calculateBill(12));  // 100
